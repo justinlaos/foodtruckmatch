1ae9e8637e2353d253ba2769fcd59dbcbe017fa0
Justin Laos <4jlaos@gmail.com>
2024-04-15 20:02:23 -0700

    removed base page html, controller, route, and tests

--- test/foodtruckmatch_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule FoodtruckmatchWeb.PageControllerTest do
-  use FoodtruckmatchWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
